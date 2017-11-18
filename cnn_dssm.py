cf364ae1b5db54202816630c832beb38de85b292
ColorLessZ <colorless@outlook.com>
2017-11-17 21:37:41 -0800

    add more summary scalar and print more info for tracking
    add more summary scalar and print more info for tracking

--- cnn_dssm.py
+++ cnn_dssm.py
@@ -13,6 +13,7 @@ from collections import OrderedDict
 from numpy.random import RandomState
 from sklearn.model_selection import KFold
 from dataprepare import load_data, build_vocab, get_idx_from_data
+from cnn_extractor import inference, calloss, training, evaluation
 
 SAVEDRI="./model"
 MAX_EPOCHS = 20
@@ -24,105 +25,10 @@ EMBEDDING = 128
 FILTERS = [2,3,4]
 NCON = 50
 GAMMA = 10
-LRATE = 0.001
+LRATE = 0.002
 SEED = 1234
 VALIDATION_RATIO = 0.1
 FOLD_COUNT = 10
-
-def inference(x, y, n_chars):
-    batch_size = x.shape[0].value
-    max_len = x.shape[1].value
-    with tf.name_scope('embedding'):
-        w_emb = tf.get_variable("c_embed", [n_chars , EMBEDDING], trainable=False)
-        
-        x_flatten_emb = tf.transpose(tf.nn.embedding_lookup(w_emb,tf.cast(tf.reshape(x,[1,(x.shape[0]*x.shape[1]).value]), dtype='int32')))
-        y_flatten_emb = tf.transpose(tf.nn.embedding_lookup(w_emb,tf.cast(tf.reshape(y,[1,(y.shape[0]*y.shape[1]).value]), dtype='int32')))
-
-        x_emb = tf.transpose(tf.reshape(x_flatten_emb,[x.shape[0].value, 1, EMBEDDING, x.shape[1].value]), [0, 2, 3, 1])
-        y_emb = tf.transpose(tf.reshape(y_flatten_emb,[y.shape[0].value, 1, EMBEDDING, y.shape[1].value]), [0, 2, 3, 1])
-    
-    with tf.name_scope('convLayers'):
-        W_conv = OrderedDict()
-        b_conv = OrderedDict()
-
-        cov_output_x = OrderedDict()
-        cov_output_y = OrderedDict()
-
-        for i in range(len(FILTERS)):
-            W_conv[i] = weight_variable([EMBEDDING, FILTERS[i], 1, FEATURE])
-            b_conv[i] = bias_variable([FEATURE])
-            cov_output_x[i] = tf.nn.relu(conv2d(x_emb, W_conv[i]) + b_conv[i])
-            cov_output_y[i] = tf.nn.relu(conv2d(y_emb, W_conv[i]) + b_conv[i])
-    
-    with tf.name_scope('maxpooling'):
-        pooling_output_x = []
-        pooling_output_y = []
-        for i in range(len(FILTERS)):
-            pooling_output_x.append(max_pool(cov_output_x[i]))
-            pooling_output_y.append(max_pool(cov_output_y[i]))
-        feature_x = tf.reshape(tf.concat(pooling_output_x, 1),[batch_size,len(FILTERS)*FEATURE])
-        feature_y = tf.reshape(tf.concat(pooling_output_y, 1),[batch_size,len(FILTERS)*FEATURE])
-        """if needed drop out here"""
-
-    return feature_x, feature_y
-
-def calloss(feature_x, feature_y):
-    with tf.name_scope('FD_rotate'):
-        # Rotate FD+ to produce 50 FD-
-        temp = tf.tile(feature_y, [1, 1])
-        x_feature = tf.tile(feature_x, [NCON + 1, 1])
-        y_feature = tf.slice(feature_y, [0, 0], [0, -1])
-        for i in range(BS):
-            rotation = tf.concat([tf.slice(temp, [i, 0], [BS - i, -1]), tf.slice(temp, [0, 0], [i, -1])], 0)
-            cy_rows = tf.gather(rotation, random.sample(range(1,BS), NCON))
-            y_feature = tf.concat([y_feature, tf.slice(rotation,[0,0],[1,-1]), cy_rows], 0)        
-    with tf.name_scope('Cosine_Similarity'):
-        # Cosine similarity
-        x_norm = tf.sqrt(tf.reduce_sum(tf.square(x_feature), 1, True))
-        y_norm = tf.sqrt(tf.reduce_sum(tf.square(y_feature), 1, True))
-
-        prod = tf.reduce_sum(tf.multiply(x_feature, y_feature), 1, True)
-        norm_prod = tf.multiply(x_norm, y_norm)
-
-        cos_sim_raw = tf.truediv(prod, norm_prod)
-        cos_sim = tf.reshape(cos_sim_raw, [BS, NCON + 1]) * GAMMA
-
-    with tf.name_scope('Loss'):
-        # Train Loss
-        # investigation on loss calculation
-        prob = tf.nn.softmax((cos_sim)) 
-        hit_prob = tf.slice(prob, [0, 0], [-1, 1]) #P_Q_D+
-        loss = -tf.reduce_sum(tf.log(hit_prob)) / BS    
-    return cos_sim, loss
-
-def training(loss, lrate):
-    with tf.name_scope('Training'):
-        tf.summary.scalar('loss', loss)
-
-        global_step = tf.Variable(0, name='global_step', trainable=False)
-        # Optimizer
-        train_op = tf.train.AdamOptimizer(lrate).minimize(loss, global_step = global_step)
-        return train_op
-
-def evaluation(cos_sim):
-    with tf.name_scope('Evaluation'):
-        npts = cos_sim.shape[0].value
-    r = []
-    for index in range(npts):
-        top_value, top_indices = tf.nn.top_k(tf.reshape(cos_sim[index],[1,cos_sim.shape[1].value]), k=cos_sim.shape[1].value)
-        where = tf.where(tf.equal(top_indices, 0))
-        result = tf.segment_min(where[:, 1], where[:, 0])
-        r.append(result)
-                
-    # Compute metrics
-    ranks = tf.reshape(r,[1,len(r)])
-    r1 = 100 * tf.count_nonzero(tf.less(ranks, 1)) / ranks.shape[1].value
-    r3 = 100 * tf.count_nonzero(tf.less(ranks, 3)) / ranks.shape[1].value
-    r10 = 100 * tf.count_nonzero(tf.less(ranks, 10)) / ranks.shape[1].value
-    medr = tf.nn.top_k(ranks, ranks.shape[1].value//2).values[0][-1] + 1
-    meanr = tf.reduce_mean(ranks) + 1
-    h_meanr = 1/tf.reduce_mean(1/(ranks+1))
-    return r1, r3, r10, medr, meanr,h_meanr
     
 def input_placeholder(batch_size, max_len):
     x_placeholder = tf.placeholder(tf.float32, shape=(batch_size, max_len))
@@ -170,7 +76,7 @@ def do_eval(sess,
   print('Valid Rank: r1: %.f, r3: %.f, r10: %.f, medr: %d, meanr: %.f, h_meanr: %.f' % (rank1/batch_count, rank3/batch_count, rank10/batch_count, med_rank/batch_count, mean_rank/batch_count,h_mean_rank/batch_count))
   return (rank1+rank3+rank10)/batch_count
 
-def run_training(train, validation, test, n_char, max_len, iteration):
+def run_training(train, validation, test, n_char, max_len, iteration, l_rate):
 
     directory = SAVEDRI + '/' + str(iteration)
     if not os.path.exists(directory):
@@ -189,13 +95,13 @@ def run_training(train, validation, test, n_char, max_len, iteration):
         x_placeholder, y_placeholder = input_placeholder(BS, max_len)
 
         #Add a Graph to compute the feature vectors.
-        feature_x, feature_y = inference(x_placeholder, y_placeholder, n_char)
+        feature_x, feature_y = inference(x_placeholder, y_placeholder, n_char, EMBEDDING, FILTERS, FEATURE)
 
         #Add Ops for loss calculation to the Graph.
-        cos_sim, loss = calloss(feature_x, feature_y)
+        cos_sim, loss = calloss(feature_x, feature_y, NCON, GAMMA)
 
         #Add Ops that calculate and apply gradients.
-        train_op = training(loss, LRATE)
+        train_op = training(loss, l_rate)
 
         #Ops of evaluation: r1+r3+r10
         r1, r3, r10, medr, meanr,h_meanr = evaluation(cos_sim)
@@ -216,7 +122,7 @@ def run_training(train, validation, test, n_char, max_len, iteration):
         sess = tf.Session(config=config)
 
         #Instantiate a summery Writer to output summaries and the Graph
-        summary_writer = tf.summary.FileWriter(SAVEDRI, sess.graph)
+        summary_writer = tf.summary.FileWriter(directory, sess.graph)
 
         #Run the Op to initialize the variables
         sess.run(init)
@@ -244,12 +150,15 @@ def run_training(train, validation, test, n_char, max_len, iteration):
 
                 #Write the summaries and print an overview
                 if np.mod(uidx+1, SUMMARY_FREQ):
-                    print('Iteration: %d: Epoch %d: loss = %.2f (%.3f sec)' % (iteration, eidx, loss_value, duration))
+                    print('(%d/%d Iteration: %d: Epoch %d) loss = %.2f (%.3f sec)' % (mini_batch, batch_count, iteration, eidx, loss_value, duration))
                     summary_str = sess.run(summary, feed_dict = feed_dict)
                     summary_writer.add_summary(summary_str, uidx)
                     summary_writer.flush()
+                
+                #Save checkpoint and evaluate the model periodically.
+                if np.mod(uidx+1, EVAL_FREQ):       
                     # Evaluate against the training set.
-                    print('Training Data Eval on iteration %d' % (iteration))
+                    print('(%d/%d Iteration: %d: Epoch %d) Training Data Eval:' % (mini_batch, batch_count, iteration, eidx))
                     _ = do_eval(sess,
                             r1, r3, r10, medr, meanr,h_meanr,
                             x_placeholder,
@@ -258,10 +167,8 @@ def run_training(train, validation, test, n_char, max_len, iteration):
                             x_minibatch,
                             y_minibatch)
 
-                #Save checkpoint and evaluate the model periodically.
-                if np.mod(uidx+1, EVAL_FREQ):                                        
                     # Evaluate against the validation set.                    
-                    print('Validation Data Eval on iteration %d' % (iteration))
+                    print('(%d/%d Iteration: %d: Epoch %d) Validation Data Eval:' % (mini_batch, batch_count, iteration, eidx))
                     r_at_10 = do_eval(sess,
                                 r1, r3, r10, medr, meanr,h_meanr,
                                 x_placeholder,
@@ -274,9 +181,10 @@ def run_training(train, validation, test, n_char, max_len, iteration):
                         print('Save model.')
                         checkpoint_file = os.path.join(directory, 'model.ckpt')
                         saver.save(sess, checkpoint_file, global_step=uidx)
-
+                    else:
+                        l_rate = 0.9*l_rate  
                     # Evaluate against the test set.
-                    print('Test Data Eval on iteration %d' % (iteration))
+                    print('(%d/%d Iteration: %d: Epoch %d) Test Data Eval:' % (mini_batch, batch_count, iteration, eidx))
                     do_eval(sess,
                             r1, r3, r10, medr, meanr,h_meanr,
                             x_placeholder,
@@ -306,7 +214,7 @@ def main(_):
 
         train, valid = create_valid(train, valid_portion=VALIDATION_RATIO) 
         i += 1       
-        r1, r3, r10, medr, meanr,h_meanr = run_training(train, valid, test, n_chars, max_len, i)
+        r1, r3, r10, medr, meanr,h_meanr = run_training(train, valid, test, n_chars, max_len, i, LRATE)
 
 def create_valid(train_set,valid_portion=VALIDATION_RATIO):
     
@@ -324,29 +232,6 @@ def create_valid(train_set,valid_portion=VALIDATION_RATIO):
     valid = (valid_set_x, valid_set_y)
 
     return train, valid
-
-def conv2d(x, W, format='NHWC'):
-    """conv2d returns a 2d convolution layer with full stride."""
-    return tf.nn.conv2d(x, W, strides=[1, x.shape[1].value, 1, 1], padding='SAME', data_format=format)
-
-def max_pool(x,format='NHWC'):
-      return tf.nn.max_pool(x, ksize=[1, 1, x.shape[2].value, 1],strides=[1, 1, x.shape[2].value, 1], padding='SAME', data_format=format)
-
-def weight_variable(shape, stddev=0.01):
-  """weight_variable generates a weight variable of a given shape."""           
-  initial = tf.truncated_normal(shape, stddev=stddev)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-      """bias_variable generates a bias variable of a given shape."""
-      initial = tf.constant(0.1, shape=shape)
-      return tf.Variable(initial)
-
-def uniform_weight(nin,nout=None, scale=1):
-    if nout == None:
-        nout = nin
-    initial = tf.random_uniform(shape=(nin, nout), minval=-scale, maxval=scale)
-    return tf.Variable(initial)
 
 if __name__=="__main__":  
     os.chdir(sys.path[0])
---
+++ cnn_extractor.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import logging
+import random
+import time
+
+import tensorflow as tf
+import numpy as np
+import _pickle as cPickle
+import scipy.stats as ss
+
+from collections import OrderedDict
+from numpy.random import RandomState
+
+def inference(x, y, n_chars, embedding_size, filters, feature_size):
+    batch_size = x.shape[0].value
+    max_len = x.shape[1].value
+    with tf.name_scope('embedding'):
+        w_emb = tf.get_variable("c_embed", [n_chars , embedding_size], trainable=False)
+        
+        x_flatten_emb = tf.transpose(tf.nn.embedding_lookup(w_emb,tf.cast(tf.reshape(x,[1,(x.shape[0]*x.shape[1]).value]), dtype='int32')))
+        y_flatten_emb = tf.transpose(tf.nn.embedding_lookup(w_emb,tf.cast(tf.reshape(y,[1,(y.shape[0]*y.shape[1]).value]), dtype='int32')))
+
+        x_emb = tf.transpose(tf.reshape(x_flatten_emb,[x.shape[0].value, 1, embedding_size, x.shape[1].value]), [0, 2, 3, 1])
+        y_emb = tf.transpose(tf.reshape(y_flatten_emb,[y.shape[0].value, 1, embedding_size, y.shape[1].value]), [0, 2, 3, 1])
+    
+    with tf.name_scope('convLayers'):
+        W_conv = OrderedDict()
+        b_conv = OrderedDict()
+
+        cov_output_x = OrderedDict()
+        cov_output_y = OrderedDict()
+
+        for i in range(len(filters)):
+            W_conv[i] = weight_variable([embedding_size, filters[i], 1, feature_size], "conv_" + str(i) + "_weight")
+            b_conv[i] = bias_variable([feature_size], "conv_" + str(i) + "_bias")
+            cov_output_x[i] = tf.nn.relu(conv2d(x_emb, W_conv[i]) + b_conv[i])
+            cov_output_y[i] = tf.nn.relu(conv2d(y_emb, W_conv[i]) + b_conv[i])
+    
+    with tf.name_scope('maxpooling'):
+        pooling_output_x = []
+        pooling_output_y = []
+        for i in range(len(filters)):
+            pooling_output_x.append(max_pool(cov_output_x[i]))
+            pooling_output_y.append(max_pool(cov_output_y[i]))
+        feature_x = tf.reshape(tf.concat(pooling_output_x, 1),[batch_size,len(filters)*feature_size])
+        feature_y = tf.reshape(tf.concat(pooling_output_y, 1),[batch_size,len(filters)*feature_size])
+        """if needed drop out here"""
+
+    return feature_x, feature_y
+
+def calloss(feature_x, feature_y, negtive_count, gamma):
+    batch_size = feature_x.shape[0].value
+    with tf.name_scope('FD_rotate'):
+        # Rotate FD+ to produce 50 FD-
+        temp = tf.tile(feature_y, [1, 1])
+        x_feature = tf.tile(feature_x, [negtive_count + 1, 1])
+        y_feature = tf.slice(feature_y, [0, 0], [0, -1])
+        for i in range(batch_size):
+            rotation = tf.concat([tf.slice(temp, [i, 0], [batch_size - i, -1]), tf.slice(temp, [0, 0], [i, -1])], 0)
+            cy_rows = tf.gather(rotation, random.sample(range(1,batch_size), negtive_count))
+            y_feature = tf.concat([y_feature, tf.slice(rotation,[0,0],[1,-1]), cy_rows], 0)        
+    with tf.name_scope('Cosine_Similarity'):
+        # Cosine similarity
+        x_norm = tf.sqrt(tf.reduce_sum(tf.square(x_feature), 1, True))
+        y_norm = tf.sqrt(tf.reduce_sum(tf.square(y_feature), 1, True))
+
+        prod = tf.reduce_sum(tf.multiply(x_feature, y_feature), 1, True)
+        norm_prod = tf.multiply(x_norm, y_norm)
+
+        cos_sim_raw = tf.truediv(prod, norm_prod)
+        cos_sim = tf.reshape(cos_sim_raw, [batch_size, negtive_count + 1]) * gamma
+    with tf.name_scope('Loss'):
+        # Train Loss
+        # investigation on loss calculation
+        prob = tf.nn.softmax((cos_sim)) 
+        hit_prob = tf.slice(prob, [0, 0], [-1, 1]) #P_Q_D+
+        loss = -tf.reduce_sum(tf.log(hit_prob)) / batch_size    
+    return cos_sim, loss
+
+def training(loss, lrate):
+    with tf.name_scope('Training'):
+        tf.summary.scalar('loss', loss)
+
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        # Optimizer
+        train_op = tf.train.AdamOptimizer(lrate).minimize(loss, global_step = global_step)
+        return train_op
+    
+def evaluation(cos_sim):
+    with tf.name_scope('Evaluation'):
+        npts = cos_sim.shape[0].value
+    r = []
+    v = []
+    for index in range(npts):
+        cos_sim_reshape = tf.reshape(cos_sim[index],[1,cos_sim.shape[1].value])
+        value = cos_sim_reshape[0][0]
+        top_value, top_indices = tf.nn.top_k(cos_sim_reshape, k=cos_sim.shape[1].value)
+        where = tf.where(tf.equal(top_indices, 0))
+        result = tf.segment_min(where[:, 1], where[:, 0])
+        r.append(result)
+        v.append(value)
+                
+    # Compute metrics
+    ranks = tf.reshape(r,[1,len(r)])
+    cos_values = tf.reshape(v,[1,len(v)])
+    r1_mask = tf.less(ranks, 1)
+    r3_mask = tf.less(ranks, 3)
+    r10_mask = tf.less(ranks, 10)
+    r1 = 100 * tf.count_nonzero(r1_mask) / ranks.shape[1].value
+    r3 = 100 * tf.count_nonzero(r3_mask) / ranks.shape[1].value
+    r10 = 100 * tf.count_nonzero(r10_mask) / ranks.shape[1].value
+    medr = tf.nn.top_k(ranks, ranks.shape[1].value//2).values[0][-1] + 1
+    meanr = tf.reduce_mean(ranks) + 1
+    h_meanr = 1/tf.reduce_mean(1/(ranks+1))
+    tf.summary.scalar('r1', r1)
+    tf.summary.scalar('r3', r3)
+    tf.summary.scalar('r10', r10)
+    
+    v1 = tf.reduce_mean(tf.boolean_mask(cos_values, r1_mask))
+    v3 = tf.reduce_mean(tf.boolean_mask(cos_values, r3_mask))
+    v10 = tf.reduce_mean(tf.boolean_mask(cos_values, r10_mask))
+    tf.summary.scalar('v1', v1)
+    tf.summary.scalar('r3', v3)
+    tf.summary.scalar('v10', v10)
+    
+    return r1, r3, r10, medr, meanr,h_meanr
+
+def conv2d(x, W, format='NHWC'):
+    """conv2d returns a 2d convolution layer with full stride."""
+    return tf.nn.conv2d(x, W, strides=[1, x.shape[1].value, 1, 1], padding='SAME', data_format=format)
+
+def max_pool(x,format='NHWC'):
+      return tf.nn.max_pool(x, ksize=[1, 1, x.shape[2].value, 1],strides=[1, 1, x.shape[2].value, 1], padding='SAME', data_format=format)
+
+def weight_variable(shape, name, stddev=0.01):
+  """weight_variable generates a weight variable of a given shape."""           
+  initial = tf.truncated_normal(shape, stddev=stddev)
+  return tf.Variable(initial, name=name)
+
+def bias_variable(shape, name):
+      """bias_variable generates a bias variable of a given shape."""
+      initial = tf.constant(0.1, shape=shape)
+      return tf.Variable(initial, name=name)
+
+def uniform_weight(nin,nout=None, scale=1):
+    if nout == None:
+        nout = nin
+    initial = tf.random_uniform(shape=(nin, nout), minval=-scale, maxval=scale)
+    return tf.Variable(initial)
